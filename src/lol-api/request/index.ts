4e96dbf4a83413e979f60691c8601de4fb0f9010
Sasha <8694395+SashaRyder@users.noreply.github.com>
2021-01-10 21:04:43 +0000

    Added logging in requester so I can see whats going on.

--- src/lol-api/request/index.ts
+++ src/lol-api/request/index.ts
@@ -1,26 +1,39 @@
-import fetch from 'electron-fetch'
-import { ApiError } from '../LeagueApiInterfaces';
+import fetch from "electron-fetch";
+import { ApiError } from "../LeagueApiInterfaces";
+import log from "electron-log";
 
-export default async (hostname: string, port: number, auth: string, endpoint: string, method: string = "GET", payload: string = null): Promise<{ response?: any, error?: ApiError }> => {
-    let headers: HeadersInit = {
-        "Accept": "application/json",
-        "Authorization": `Basic ${Buffer.from(`riot:${auth}`).toString("base64")}`
-    };
-    if (payload) {
-        headers = { ...headers, ["Content-Type"]: "application/json" };
+export default async (
+  hostname: string,
+  port: number,
+  auth: string,
+  endpoint: string,
+  method = "GET",
+  payload: string = null
+): Promise<{ response?: never | null; error?: ApiError }> => {
+  let headers: HeadersInit = {
+    Accept: "application/json",
+    Authorization: `Basic ${Buffer.from(`riot:${auth}`).toString("base64")}`,
+  };
+  if (payload) {
+    headers = { ...headers, ["Content-Type"]: "application/json" };
+  }
+  try {
+    const reqEndpoint = `https://${hostname}:${port}${
+      endpoint.startsWith("/") ? "" : "/"
+    }${endpoint}`;
+    log.info(`Hitting API at: ${reqEndpoint}`);
+    const data = await fetch(reqEndpoint, {
+      headers,
+      method,
+      body: payload,
+    }).then((val) => val.json());
+    if (data.errorCode) {
+      log.error(data);
+      return { response: null, error: data };
     }
-    try {
-        const data = await fetch(`https://${hostname}:${port}${endpoint.startsWith("/") ? "" : "/"}${endpoint}`, {
-            headers,
-            method,
-            body: payload
-        }).then(val => val.json());
-        if (data.errorCode) {
-            return { response: null, error: data };
-        }
-        return { response: data, error: null };
-    }
-    catch (error) {
-        return { response: null, error }
-    }
-}
+    return { response: data, error: null };
+  } catch (error) {
+    log.error(error);
+    return { response: null, error };
+  }
+};
